cea2dc4644f95a7863b711eea7a1b1e13f409eb9
flyx-here <danielkendek305@gmail.com>
2024-07-17 19:58:07 +0900

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'For Prettiest Girl Rifa <3 '.split('')
+const text = 'The eternal flower for U'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
